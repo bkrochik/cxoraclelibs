470c7d8add828722596660773bfce6cf4b991c46
bkrochik <brian.krochik@mercadolibre.com>
2013-08-29 12:04:00 -0300

    Fix

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 echo "Starting........."
 
-mkdir /usr/local/oracle
+mkdir /usr/local/cxoracle
 
-cp -f libs/instantclient-basic-macos.x32-11.2.0.3.0.zip /usr/local/oracle
-cp -f libs/instantclient-sdk-macos.x32-11.2.0.3.0.zip /usr/local/oracle
-cp -f libs/cx_Oracle-5.1.2.zip /usr/local/oracle
+cp -f libs/instantclient-basic-macos.x32-11.2.0.3.0.zip /usr/local/cxoracle
+cp -f libs/instantclient-sdk-macos.x32-11.2.0.3.0.zip /usr/local/cxoracle
+cp -f libs/cx_Oracle-5.1.2.zip /usr/local/cxoracle
 
-cd /usr/local/oracle 
+cd /usr/local/cxoracle 
 
-unzip /usr/local/oracle/instantclient-basic-macos.x32-11.2.0.3.0.zip
-unzip /usr/local/oracle/instantclient-sdk-macos.x32-11.2.0.3.0.zip
-unzip /usr/local/oracle/cx_Oracle-5.1.2.zip 
+unzip /usr/local/cxoracle/instantclient-basic-macos.x32-11.2.0.3.0.zip
+unzip /usr/local/cxoracle/instantclient-sdk-macos.x32-11.2.0.3.0.zip
+unzip /usr/local/cxoracle/cx_Oracle-5.1.2.zip 
 
-ln -s /usr/local/oracle/instantclient_11_2/libclntsh.dylib.11.1 /usr/local/oracle/instantclient_11_2/libclntsh.dylib
-ln -s /usr/local/oracle/instantclient_11_2/libocci.dylib.11.1 /usr/local/oracle/instantclient_11_2/libocci.dylib
+ln -s /usr/local/cxoracle/instantclient_11_2/libclntsh.dylib.11.1 /usr/local/cxoracle/instantclient_11_2/libclntsh.dylib
+ln -s /usr/local/cxoracle/instantclient_11_2/libocci.dylib.11.1 /usr/local/cxoracle/instantclient_11_2/libocci.dylib
 
 if ! grep -q ORACLE_HOME ~/.bash_profile; 
   then
-    echo "export ORACLE_HOME=/usr/local/oracle/instantclient_11_2" >> ~/.bash_profile
+    echo "export ORACLE_HOME=/usr/local/cxoracle/instantclient_11_2" >> ~/.bash_profile
 	echo "export LD_LIBRARY_PATH=$ORACLE_HOME" >> ~/.bash_profile
 	echo "export DYLD_LIBRARY_PATH=$ORACLE_HOME" >> ~/.bash_profile
 	echo "export VERSIONER_PYTHON_PREFER_32_BIT=yes" >> ~/.bash_profile
@@ -27,11 +27,11 @@ if ! grep -q ORACLE_HOME ~/.bash_profile;
 fi
  
 
-sudo python /usr/local/oracle/cx_Oracle-5.1.2/setup.py build
-sudo python /usr/local/oracle/cx_Oracle-5.1.2/setup.py install
+sudo python /usr/local/cxoracle/cx_Oracle-5.1.2/setup.py build
+sudo python /usr/local/cxoracle/cx_Oracle-5.1.2/setup.py install
 
-rm /usr/local/oracle/instantclient-basic-macos.x32-11.2.0.3.0.zip
-rm /usr/local/oracle/instantclient-sdk-macos.x32-11.2.0.3.0.zip
-rm /usr/local/oracle/cx_Oracle-5.1.2.zip
+rm /usr/local/cxoracle/instantclient-basic-macos.x32-11.2.0.3.0.zip
+rm /usr/local/cxoracle/instantclient-sdk-macos.x32-11.2.0.3.0.zip
+rm /usr/local/cxoracle/cx_Oracle-5.1.2.zip
 
 echo "Finished........."
