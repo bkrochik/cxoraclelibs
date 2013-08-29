a657681f2c98a48d0ed6784f2c9509c2769f344a
bkrochik <brian.krochik@mercadolibre.com>
2013-08-29 11:44:51 -0300

    Fix

--- scripts/triggerChargesNW.py
+++ scripts/triggerChargesNW.py
@@ -2,8 +2,6 @@ import random
 import unittest
 import cx_Oracle
 
-#Install pip install cx_Oracle
-
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
@@ -18,12 +16,17 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_query(self):
         cur = self.db.cursor()
+        #Consultas
         cur.execute('select * from feed_charges where rownum=1')
         resultset = cur.fetchall()
         for result in resultset:
             self.assertEqual(168,result[0])
         
-
+    def test_insert(self):
+        cur=self.db.cursor()
+        #Example insert 1) .execute(INSERTQUERY) 2) .commit()
+        self.assertTrue(True)
+        
 
 if __name__ == '__main__':
     unittest.main()
