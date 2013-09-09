092d666613e55f01d2ba181c50b8ed01df390c25
bkrochik <brian.krochik@mercadolibre.com>
2013-09-09 17:18:57 -0300

    Fix tests

--- tests/test-all.py
+++
@@ -1,9 +0,0 @@
-def load_tests(loader, tests, pattern):
-    suite = TestSuite()
-    for all_test_suite in unittest.defaultTestLoader.discover('tests', pattern='*.py'):
-        for test_suite in all_test_suite:
-            suite.addTests(test_suite)
-    return suite
-
-if __name__ == '__main__':
-    unittest.main()
